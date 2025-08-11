c47fffa916ca9b1903511eddfeddab792d2b1858
Manoj.V <venkimanoj2@gmail.com>
2025-08-11 17:40:45 +0530

    [Feature] Added SSR and SSG

---
+++ apps/vedic-astrology-matching/src/main.server.ts
@@ -0,0 +1,22 @@
+import {
+  bootstrapApplication,
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
+import { AppComponent } from '../src/app/app.component';
+import { provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { appRoutes } from './app/app.routes';
+import { provideServerRendering } from '@angular/platform-server';
+
+const bootstrap = () =>
+  bootstrapApplication(AppComponent, {
+    providers: [
+      provideExperimentalZonelessChangeDetection(),
+      provideRouter(appRoutes),
+      provideClientHydration(withEventReplay()),
+      provideServerRendering(),
+    ],
+  });
+
+export default bootstrap;
