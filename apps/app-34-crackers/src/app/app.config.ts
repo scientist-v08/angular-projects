f9a9e4e3c4af18cdedc5fcde58ddc7c679033425
Manoj.V <venkimanoj2@gmail.com>
2025-11-09 15:46:05 +0530

    [Feature] Added Login and Logout feature along with admin and user roles

--- apps/app-34-crackers/src/app/app.config.ts
+++ apps/app-34-crackers/src/app/app.config.ts
@@ -5,11 +5,14 @@ import {
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { AuthInterceptor } from './services/interceptor.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideClientHydration(),
     provideExperimentalZonelessChangeDetection(),
+    provideHttpClient(withInterceptors([AuthInterceptor])),
     provideRouter(appRoutes),
   ],
 };
